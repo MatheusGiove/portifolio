12c8b9de0fe2d351e99dbc8b1b693ee5399ae1c0
Matheus Faria <123787187+MatheusGiove@users.noreply.github.com>
2023-11-24 15:46:28 -0300

    update homepage

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import styles from "./Home.module.css";
 import Typed from "react-typed";
 import Logo from "../../Assets/svg/Logo";
+import LinkedinIcon from "../../Assets/svg/contact/LinkedinIcon";
+import GithubIcon from "../../Assets/svg/contact/GithubIcon";
+import InstagramIcon from "../../Assets/svg/contact/InstagramIcon";
 
 const Home = () => {
   return (
@@ -16,15 +19,28 @@ const Home = () => {
           typeSpeed={50}
           loop
           backSpeed={30}
-          strings={[
-            "Desenvolvedor Front-End",
-            "Web Designer",
-            "Freelancer",
-          ]}
+          strings={["Desenvolvedor Front-End", "Web Designer", "Freelancer"]}
           backDelay={1000}
           cursorChar="|"
         />
       </h2>
+      <ul className={styles.list}>
+        <li className={styles.contact}>
+          <a target="_blank" href="https://www.linkedin.com/in/matheus-faria-giove-3a6159259/">
+            <LinkedinIcon />
+          </a>
+        </li>
+        <li className={styles.contact}>
+          <a target="_blank" href="https://github.com/MatheusGiove">
+            <GithubIcon />
+          </a>
+        </li>
+        <li className={styles.contact}>
+          <a target="_blank" href="https://www.instagram.com/matheusgiove/">
+            <InstagramIcon />
+          </a>
+        </li>
+      </ul>
     </section>
   );
 };
